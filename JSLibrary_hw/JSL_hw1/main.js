d5eb44077768f913e4185c5359e085901d6e2bdb
Berezka-creator <61727413+Berezka-creator@users.noreply.github.com>
2020-08-28 20:37:48 -0700

    Add files via upload

---
+++ JSLibrary_hw/JSL_hw1/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("h2").each(function(i,e){
+      $(this).attr("id", "heading_"+i);
+      $("ul").append($("<li><a href='#" +$(this).attr("id")+"'/>"+$(this).text()+"</li>"));
+
+  });
+});
+
+
+$(document).ready(function(){
+  $('li a').click(function(e){
+      if(this.hash !==''){
+      e.preventDefault();
+      const hash = this.hash;
+      $('body,html').animate({scrollTop:$(hash).offset().top}, 500);};
+  })
+});
+       
