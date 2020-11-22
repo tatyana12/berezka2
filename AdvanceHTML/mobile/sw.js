10f558c0c98133b0c6605829c81c5639edc35264
berezka-creator <berezina.y.v.2015@gmail.com>
2020-11-22 13:38:07 -0800

    PWA

--- AdvanceHTML/mobile/sw.js
+++ AdvanceHTML/mobile/sw.js
@@ -1,17 +1,4 @@
-/*const staticAssets=[
-    './',
-    './style.css',
-    './app.js',
-    'images/lightblue.jpg',
-    'images/lightgold.jpg',
-    'images/icon-192x192.png',
-    'images/icon-256x256.png',
-    'images/icon-384x384.png',
-    'images/icon-512x512.png',
-    'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
-    'http://code.jquery.com/jquery-1.7.1.js'
-];
-*/
+
 self.addEventListener('install', async event =>{
 
 
