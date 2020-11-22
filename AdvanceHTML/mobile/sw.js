4822e26a939a86186f0cca154800f29ac47932b0
berezka-creator <berezina.y.v.2015@gmail.com>
2020-11-22 13:34:49 -0800

    PWA

--- AdvanceHTML/mobile/sw.js
+++ AdvanceHTML/mobile/sw.js
@@ -1,4 +1,4 @@
-const staticAssets=[
+/*const staticAssets=[
     './',
     './style.css',
     './app.js',
@@ -11,40 +11,14 @@ const staticAssets=[
     'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
     'http://code.jquery.com/jquery-1.7.1.js'
 ];
-
+*/
 self.addEventListener('install', async event =>{
-    const cache = await caches.open('pwa-static');
+
 
     console.log('install');
 
 });
 self.addEventListener('fetch', event =>{
-    const req=event.request;
-    const url = new URL (req.url);
 
-    if(url.origin === location.origin){
-        event.respondWith(cacheFirst(req));
-    }else{
-        event.respondWith(networkFirst(req));
-    }
-
+    console.log("fetch");
 });
-async function cacheFirst(req){
-    const cachedResponse = await caches.match(req);
-    return cachedResponse || fetch(req);
-}
-
-async function networkFirst(req){
-    const cache = await caches.open('pwa-dynamic');
-
-
-    try{
-        const res = await fetch(req);
-        cache.put(req, res.clone());
-        return res;
-
-    }catch(error){
-        return await cache.match(req);
-    }
-}
-//
