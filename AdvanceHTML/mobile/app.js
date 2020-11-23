f20f1a75e19183e935b829e6772a05898f160467
berezka-creator <berezina.y.v.2015@gmail.com>
2020-11-22 22:55:46 -0800

    PWA

--- AdvanceHTML/mobile/app.js
+++ AdvanceHTML/mobile/app.js
@@ -11,17 +11,32 @@ window.addEventListener('load', async e =>{
 });
 
 let deferredPrompt;
+let btnAdd = document.querySelector(".button");
 
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent the mini-infobar from appearing on mobile
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
+    btnAdd.style.display = "block";
     // Update UI notify the user they can install the PWA
     showInstallPromotion();
 });
 
-
+btnAdd.addEventListener('click', (e) => {
+    // Hide the app provided install promotion
+    hideMyInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+    });
+});
 
 $('ul.dropdown ul').hide();
 $( "ul.dropdown li" ).hover(
